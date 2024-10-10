f5a22a140e3fc0e2c8229b8f577b4eefea842dac
Lucía Parrilla del Río <luciaparrilla9@hotmail.com>
2024-10-10 17:31:31 +0200

    Ejercicio 1

--- src/main/java/com/campusdual/classroom/Exercise01.java
+++ src/main/java/com/campusdual/classroom/Exercise01.java
@@ -3,6 +3,7 @@ package com.campusdual.classroom;
 public class Exercise01 {
 
     public static void main(String[] args) {
+        System.out.println("Hello, World!");
         //TODO → Write: System.out.println("Hello, World!");
     }
 }
